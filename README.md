7edb8de0473397125cc3fd8acf17f2432640004f
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-06-22 23:32:55 +0900

    constant and variable
    how to description constant and cariable

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # swift_basic
 IOS개발자가 되기 위하여 swift를 공부하고 기초 문법을 정리
+
+# 변수와 상수 선언
+
+## 변수(variable)
+변수는 항상 변할 수 있는 값  
+선언하는 방법
+
+```
+var hello = "hello" 
+var world : String = "world!"
+
+print(type(of:a)) // String
+print(type(of:b)) // String
+```
+
+## 상수(constant)
+상수는 변할 수 없는 값. 
+선언하는 방법  
+```
+let hello = 1
+let hello : int = 2
+```
+
---
+++ constant,variable.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+//변수 선언
+var greeting = "Hello, playground"
+var version = 1.0
+let year = 2021
+let handsome = true
+
+//변수 type 지정
+var greeting2:String = "Hello, playground"
+var version2:Double = 1.0
+let year2:Int = 2021
+let handsome2:Bool = true
+
+//변수type 확인하는 명령어
+print(type(of:greeting))
+print(greeting)
+
+
+print(type(of:version))
+print(version)
+
+print(type(of:year))
+print(year)
+
+print(type(of:handsome))
+print(handsome)
